e363b96e58f9f2e872e2d7c9f5b74f79518cf9fd
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2020-07-12 19:20:22 -0400

    save

--- lib/widgets/headers.dart
+++ lib/widgets/headers.dart
@@ -3,9 +3,14 @@ import 'package:flutter/material.dart';
 class HeaderSquare extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      color: Color(0xff212121),
+    return Scaffold(
+      body: Container(
+        height: 300,
+        color: Color(0xff212121),
+        child: Center(
+          child: Text("Header Square"),
+        ),
+      ),
     );
   }
 }
