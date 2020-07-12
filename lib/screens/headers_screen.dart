31cd246db45ac5364ef42981991f44cad93bef94
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2020-07-12 19:16:28 -0400

    save

--- lib/screens/headers_screen.dart
+++ lib/screens/headers_screen.dart
@@ -1,12 +1,11 @@
+import 'package:designs_app/widgets/headers.dart';
 import 'package:flutter/material.dart';
 
 class HeadersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("sfdsdf"),
-      ),
+      body: HeaderSquare(),
     );
   }
 }
