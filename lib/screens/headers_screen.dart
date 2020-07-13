21cc61eb9e0bf95c9a22e5f099d4d2509b928de1
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2020-07-12 20:33:37 -0400

    save

--- lib/screens/headers_screen.dart
+++ lib/screens/headers_screen.dart
@@ -29,13 +29,16 @@ class _HeadersScreenState extends State<HeadersScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
           _header[_current],
-          Divider(),
-          RaisedButton(
-            child: Text("Siguiente"),
-            color: Theme.of(context).accentColor,
-            onPressed: _nextHeader,
+          Container(
+            margin: EdgeInsets.only(bottom: 100),
+            child: RaisedButton(
+              child: Text("Siguiente"),
+              color: Theme.of(context).accentColor,
+              onPressed: _nextHeader,
+            ),
           )
         ],
       ),
